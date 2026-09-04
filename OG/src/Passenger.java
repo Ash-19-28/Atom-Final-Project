3645e2caa611d8e1fd26ace598dac591e6d051cd
Owner <justlearnabhishek@gmail.com>
2026-09-04 09:10:18 +0530

    Added custom exception Handling

---
+++ OG/src/Passenger.java
@@ -0,0 +1,11 @@
+class Passenger {
+    int ticketId;
+    String name;
+    int seatNumber;
+
+    public Passenger(int ticketId, String name, int seatNumber) {
+        this.ticketId = ticketId;
+        this.name = name;
+        this.seatNumber = seatNumber;
+    }
+}
